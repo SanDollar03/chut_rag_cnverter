e88fb140b62cad84e3cbb86c90644e3f1720fee5
Akira Takasugi <hardminimal_nekomi@outlook.com>
2026-02-23 22:32:53 +0900

    Delete run_net.bat

--- run_net.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\PJ\python_venv\chutto_gpt_v2
-echo 仮想環境を有効化中...
-call .\Scripts\activate.bat
-echo Flask アプリを起動中...
-cd /d C:\Users\PJ\python_venv\chutto_rag_cnverter
-python app.py
-pause
